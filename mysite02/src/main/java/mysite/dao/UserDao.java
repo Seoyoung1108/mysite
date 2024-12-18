caca43594e447b1115880652f0862aa4a0a91dad
BIT <BIT@BIT1>
2024-12-18 16:26:34 +0900

    servlet practice 12/18

---
+++ mysite02/src/main/java/mysite/dao/UserDao.java
@@ -0,0 +1,51 @@
+package mysite.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import mysite.vo.UserVo;
+
+public class UserDao {
+	public int insert(UserVo vo) {
+		int count =0;
+		
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("insert into user values (null,?,?,?,?,curdate())");				
+		) {
+			// 4. parameter binding
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getEmail());
+			pstmt.setString(3, vo.getPassword());
+			pstmt.setString(4, vo.getGender());
+			
+			// 5. SQL 실행
+			count = pstmt.executeUpdate(); // 데이터 변경
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} 
+		
+		return count;
+		
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		
+		try {
+		// 1. JDBC Driver 로딩
+		Class.forName("org.mariadb.jdbc.Driver");
+		
+		// 2. 연결하기
+		String url = "jdbc:mariadb://192.168.0.153:3306/webdb";
+		conn = DriverManager.getConnection(url, "webdb", "webdb");	
+		
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		}
+		
+		return conn;
+	}
+}
