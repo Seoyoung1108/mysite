e65c0983ef0b2f4a46f54a931ece3bf471345294
BIT <BIT@BIT1>
2024-12-23 18:16:35 +0900

    servlet practice 페이징이랑 조회수 빼고 12/23

--- mysite02/src/main/java/mysite/controller/action/board/ModifyFormAction.java
+++ mysite02/src/main/java/mysite/controller/action/board/ModifyFormAction.java
@@ -6,15 +6,27 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import mysite.controller.ActionServlet.Action;
 import mysite.dao.BoardDao;
 import mysite.vo.BoardVo;
+import mysite.vo.UserVo;
 
 public class ModifyFormAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if(session==null) {
+			response.sendRedirect(request.getContextPath());
+			return;
+		}
 		
+		UserVo authUser = (UserVo)session.getAttribute("authUser");
+		if(authUser==null) {
+			response.sendRedirect(request.getContextPath());
+			return;
+		}
 		
 		
 		Long id = Long.parseLong(request.getParameter("n"));
