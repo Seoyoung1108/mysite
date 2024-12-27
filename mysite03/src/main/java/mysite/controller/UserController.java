5fc3d6716148091899a0419fbbb06037b334c713
BIT <BIT@BIT1>
2024-12-27 14:27:25 +0900

    spring mysite 12/27

--- mysite03/src/main/java/mysite/controller/UserController.java
+++ mysite03/src/main/java/mysite/controller/UserController.java
@@ -2,18 +2,46 @@ package mysite.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import mysite.service.UserService;
+import mysite.vo.UserVo;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 	private UserService userService;
 	
 	public UserController(UserService userService) {
 		this.userService=userService;
 	}
-	@RequestMapping("/join")
+	
+	@RequestMapping(value="/join", method=RequestMethod.GET)
 	public String join() {
 		return "user/join";
 	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		userService.join(vo);
+		return "redirect:/user/joinsuccess";
+	}
+	
+	@RequestMapping("/joinsuccess")
+	public String joinSuccess() {
+		return "user/joinsuccess";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login() {
+		return "user/login";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(UserVo vo) {
+		userService.login(vo);
+		return "redirect:/";
+	}
 }
+
--- mysite03/src/main/java/mysite/service/UserService.java
+++ mysite03/src/main/java/mysite/service/UserService.java
@@ -3,6 +3,7 @@ package mysite.service;
 import org.springframework.stereotype.Service;
 
 import mysite.repository.UserRepository;
+import mysite.vo.UserVo;
 
 @Service
 public class UserService {
@@ -10,5 +11,15 @@ public class UserService {
 	
 	public UserService(UserRepository userRepository) {
 		this.userRepository=userRepository;
+	}
+
+	public void join(UserVo vo) {
+		userRepository.insert(vo);
+		
+	}
+
+	public void login(UserVo vo) {
+		// TODO Auto-generated method stub
+		
 	}
 }
