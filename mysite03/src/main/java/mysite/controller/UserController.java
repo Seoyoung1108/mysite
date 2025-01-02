8dd7053b8db67f6a73e33c9804663dce03061a40
BIT <BIT@BIT1>
2025-01-02 13:52:26 +0900

    slf4j 01/02

--- mysite03/src/main/java/mysite/controller/UserController.java
+++ mysite03/src/main/java/mysite/controller/UserController.java
@@ -28,9 +28,7 @@ public class UserController {
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
 	public String join(UserVo vo) {
-		System.out.println(vo);
 		userService.join(vo);
-		System.out.println(vo);
 		return "redirect:/user/joinsuccess";
 	}
 	
