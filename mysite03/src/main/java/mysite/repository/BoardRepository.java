7de04b0f5f05c2cc58ec31778f14dcb77080aff0
BIT <BIT@BIT1>
2025-01-02 09:26:41 +0900

    mysite03 mybatis 01/02

--- mysite03/src/main/java/mysite/repository/BoardRepository.java
+++ mysite03/src/main/java/mysite/repository/BoardRepository.java
@@ -41,127 +41,13 @@ public class BoardRepository {
 	}
 	
 	public int findAll(){
-		List<BoardVo> result = new ArrayList<>();
-		int count =0;
-		
-		try (
-				Connection conn = getConnection();
-				PreparedStatement pstmt = conn.prepareStatement("select b.id, b.title, b.contents, b.hit, date_format(b.reg_date, '%Y-%m-%d %h:%i:%s'), b.g_no, b.o_no, b.depth, b.user_id, u.name"
-						+" from board as b, user as u"
-						+" where b.user_id = u.id"
-						+" order by g_no desc, o_no asc");	
-			) {
-
-			// 5. SQL 실행
-			ResultSet rs=pstmt.executeQuery();
-			rs.last();
-			count= rs.getRow();
-
-			// 6. 결과 처리
-			while(rs.next()) {
-				Long id = rs.getLong(1);
-				String title = rs.getString(2);
-				String contents = rs.getString(3);
-				Long hit = rs.getLong(4);
-				String regDate = rs.getString(5);
-				Long gNo = rs.getLong(6);
-				Long oNo = rs.getLong(7);
-				Long depth = rs.getLong(8);
-				Long userId = rs.getLong(9);
-				String userName = rs.getString(10);
-
-				BoardVo vo = new BoardVo();
-				vo.setId(id);
-				vo.setTitle(title);
-				vo.setContents(contents);
-				vo.setHit(hit);
-				vo.setRegDate(regDate);
-				vo.setGNo(gNo);
-				vo.setONo(oNo);
-				vo.setDepth(depth);
-				vo.setUserId(userId);
-				vo.setUserName(userName);
-				result.add(vo);
-			}
-			
-			rs.close();
-			
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-		
-		return count;
+		List<BoardVo> result = sqlSession.selectList("board.findAll");
+		return result.size();
 	}
 	
 	public List<BoardVo> findByPage(int page){
-		List<BoardVo> result = new ArrayList<>();
-		
-		try (
-				Connection conn = getConnection();
-				PreparedStatement pstmt = conn.prepareStatement("select b.id, b.title, b.contents, b.hit, date_format(b.reg_date, '%Y-%m-%d %h:%i:%s'), b.g_no, b.o_no, b.depth, b.user_id, u.name"
-						+" from board as b, user as u"
-						+" where b.user_id = u.id"
-						+" order by g_no desc, o_no asc"
-						+" limit ?, 5");	
-			) {
-			
-			// 4. parameter binding
-			pstmt.setInt(1,0+5*(page-1));
-
-			// 5. SQL 실행
-			ResultSet rs=pstmt.executeQuery();
-			
-			// 6. 결과 처리
-			while(rs.next()) {
-				Long id = rs.getLong(1);
-				String title = rs.getString(2);
-				String contents = rs.getString(3);
-				Long hit = rs.getLong(4);
-				String regDate = rs.getString(5);
-				Long gNo = rs.getLong(6);
-				Long oNo = rs.getLong(7);
-				Long depth = rs.getLong(8);
-				Long userId = rs.getLong(9);
-				String userName = rs.getString(10);
-
-				BoardVo vo = new BoardVo();
-				vo.setId(id);
-				vo.setTitle(title);
-				vo.setContents(contents);
-				vo.setHit(hit);
-				vo.setRegDate(regDate);
-				vo.setGNo(gNo);
-				vo.setONo(oNo);
-				vo.setDepth(depth);
-				vo.setUserId(userId);
-				vo.setUserName(userName);
-				result.add(vo);
-			}
-			
-			rs.close();
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-		
-		return result;
-	}
-	
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-		
-		try {
-		// 1. JDBC Driver 로딩
-		Class.forName("org.mariadb.jdbc.Driver");
-		
-		// 2. 연결하기
-		String url = "jdbc:mariadb://192.168.0.153:3306/webdb";
-		conn = DriverManager.getConnection(url, "webdb", "webdb");	
-		
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		}
-		
-		return conn;
+		int param=0+5*(page-1);
+		return sqlSession.selectList("board.findByPage",param);
 	}
 
 	public BoardVo findById(Long id) {
