cdb63beb0ab333a90a8aa242e999fb0bf8ebf026
BIT <BIT@BIT1>
2025-01-06 16:55:17 +0900

    mysite03 aop 01/06

---
+++ mysite03/src/main/java/mysite/aspect/SiteAspect.java
@@ -0,0 +1,25 @@
+package mysite.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Component
+@Aspect
+public class SiteAspect {
+	@Around("execution(* mysite.repository.*.*(..))")
+	public Object adviceAround(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("-- Around Advice: Before --");
+		StopWatch sw = new StopWatch();
+		sw.start();
+		Object result = pjp.proceed();
+		sw.stop();
+		long totalTime = sw.getTotalTimeMillis();
+
+		System.out.println("[Execution Time]["+pjp.getTarget().getClass().getName()+"."+pjp.getSignature().getName()+"] "+totalTime+"millis");
+		
+		return result;
+	}
+}
--- mysite03/src/main/java/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/mysite/repository/UserRepository.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import mysite.vo.UserVo;
 
