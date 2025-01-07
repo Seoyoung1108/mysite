0595b19532ccbce8c2082d503bc557dc8a8afe5a
BIT <BIT@BIT1>
2025-01-07 16:12:36 +0900

    mysite03 validation 01/07

--- mysite03/src/main/java/mysite/controller/UserController.java
+++ mysite03/src/main/java/mysite/controller/UserController.java
@@ -1,12 +1,17 @@
 package mysite.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import mysite.security.Auth;
 import mysite.security.AuthUser;
 import mysite.service.UserService;
@@ -22,13 +27,21 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.GET)
-	public String join() {
+	public String join(@ModelAttribute UserVo userVo) {
 		return "user/join";
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVo vo) {
-		userService.join(vo);
+	public String join(@ModelAttribute @Valid UserVo userVo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			Map<String,Object> map = result.getModel();
+			model.addAllAttributes(map);
+			
+			return "user/join";
+		}
+		
+		userService.join(userVo);
+		
 		return "redirect:/user/joinsuccess";
 	}
 	
--- mysite03/src/main/java/mysite/vo/UserVo.java
+++ mysite03/src/main/java/mysite/vo/UserVo.java
@@ -1,13 +1,31 @@
 package mysite.vo;
 
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotEmpty;
+
 public class UserVo {
 	private Long id;
+	
+	@NotEmpty
 	private String name;
+	
+	@NotEmpty
 	private String email;
+	
+	@NotEmpty
+	@Length(min=4,max=8)
 	private String password;
+	
+	@NotEmpty
 	private String gender;
+	
 	private String joinDate;
 	private String role;
+	
+	@NotEmpty
+	private String agreeProv;
 	
 	public String getRole() {
 		return role;
@@ -52,6 +70,12 @@ public class UserVo {
 		this.joinDate = joinDate;
 	}
 	
+	public String getAgreeProv() {
+		return agreeProv;
+	}
+	public void setAgreeProv(String agreeProv) {
+		this.agreeProv = agreeProv;
+	}
 	@Override
 	public String toString() {
 		return "UserVo [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", gender="
